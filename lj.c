26c90ee9ccc1b5aeec354e8d680b453fbc97a8a8
Raphaël Rigo <devel-git@syscall.eu>
2018-11-18 19:26:24 +0100

    encoder

--- lj.c
+++ lj.c
@@ -12,8 +12,26 @@ void hpOutputBie(unsigned char *start, size_t len, void *output)
 
 int hp_encode_bits_to_jbig(int iWidth, int iHeight, unsigned char **pBuff, HPLJZjcBuff *pOutBuff, HPLJZjsJbgEncSt *pJbgEncSt)
 {
-    struct jbg_enc_state *s = NULL;
+    struct jbg_enc_state s;
 
-    jbg_enc_init(s, iWidth, iHeight, 1, pBuff, hpOutputBie, pOutBuff);
+    /* Encode output */
+    jbg_enc_init(&s, iWidth, iHeight, 1, pBuff, hpOutputBie, pOutBuff);
+    jbg_enc_options(&s, JBG_SMID|JBG_ILEAVE,
+                    JBG_LRLTWO|JBG_TPDON|JBG_TPBON|JBG_DPON,
+                    0x80, 0, 0);
+    jbg_enc_out(&s);
+    jbg_enc_free(&s);
+
+    /* Copy encoder state to wrapper */
+    pJbgEncSt->d = s.d;
+    pJbgEncSt->xd = s.xd;
+    pJbgEncSt->dl = s.dl;
+    pJbgEncSt->l0 = s.l0;
+    pJbgEncSt->order = s.order;
+    pJbgEncSt->planes = s.planes;
+    pJbgEncSt->options = s.options;
+    pJbgEncSt->mx = s.mx;
+    pJbgEncSt->my = s.my;
+
     return 0;
 }
