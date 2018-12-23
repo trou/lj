dcb7c8aa2a1bd54d6815d9cd03784622aa7b04fe
Raphaël Rigo <devel@syscall.eu>
2018-12-23 20:30:16 +0100

    add CRC32 output to test_jpeg

--- test_jpeg.c
+++ test_jpeg.c
@@ -9,6 +9,41 @@
 #include <stdint.h>
 #include "hpjbig_wrapper.h"
 
+uint32_t
+rc_crc32(uint32_t crc, const unsigned char *buf, size_t len)
+{
+    static uint32_t table[256];
+    static int have_table = 0;
+    uint32_t rem;
+    uint8_t octet;
+    int i, j;
+    const unsigned char *p, *q;
+
+    /* This check is not thread safe; there is no mutex. */
+    if (have_table == 0) {
+        /* Calculate CRC table. */
+        for (i = 0; i < 256; i++) {
+            rem = i;  /* remainder from polynomial division */
+            for (j = 0; j < 8; j++) {
+                if (rem & 1) {
+                    rem >>= 1;
+                    rem ^= 0xedb88320;
+                } else
+                    rem >>= 1;
+            }
+            table[i] = rem;
+        }
+        have_table = 1;
+    }
+
+    crc = ~crc;
+    q = buf + len;
+    for (p = buf; p < q; p++) {
+        octet = *p;  /* Cast to unsigned octet. */
+        crc = (crc >> 8) ^ table[(crc & 0xff) ^ octet];
+    }
+    return ~crc;
+}
 
 int (*HPJetReadyCompress) (uint8_t     *pCompressedData,
                            uint32_t    *pCompressedDataLen,
@@ -81,6 +116,7 @@ int main(int argc, char *argv[])
     res = HPJetReadyCompress(output_buffer, &out_buffer_size, input_mmap, width, height);
     printf("%d\n", res);
     printf("out_buffer_size = %u\n", out_buffer_size);
+    printf("CRC32: %08x\n", rc_crc32(0, output_buffer, out_buffer_size));
 
     if(out_buffer_size < 0x80000000) {
         out_fd = open(argv[5], O_RDWR|O_TRUNC|O_CREAT, 0600);
